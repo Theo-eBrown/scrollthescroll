0fabf2b2e5c2cb149a9cafdf0526ce6df98741df
Theo-eBrown <Theo.elliott.brown@gmail.com>
2021-10-11 23:53:24 +0100

    initialize version 0.0.7

--- src/scrollthescroll/__main__.py
+++ src/scrollthescroll/__main__.py
@@ -5,3 +5,15 @@ Created on Thu Aug 26 17:37:50 2021
 @author: Theo-eBrown
 """
 
+import __init__
+import sys
+
+if __name__ == "__main__":
+    if sys.argv[-1] == "-h":
+        #help for running
+        pass
+    else:
+        try:
+            __init__.Prototype().run(sys.argv[1],int(sys.argv[2]))
+        except IndexError:
+            raise SyntaxError("Incorrect syntax, for help run 'scrollthescroll -h'")

97e15386c16a3ea6061666b903c8108486137955
Theo-eBrown <Theo.elliott.brown@gmail.com>
2022-01-26 23:02:31 +0000

    quickfix

--- src/scrollthescroll/__main__.py
+++ src/scrollthescroll/__main__.py
@@ -4,16 +4,10 @@ Created on Thu Aug 26 17:37:50 2021
 
 @author: Theo-eBrown
 """
+import sys, os
+from scrollthescroll import Prototype
 
-import __init__
-import sys
 
-if __name__ == "__main__":
-    if sys.argv[-1] == "-h":
-        #help for running
-        pass
-    else:
-        try:
-            __init__.Prototype().run(sys.argv[1],int(sys.argv[2]))
-        except IndexError:
-            raise SyntaxError("Incorrect syntax, for help run 'scrollthescroll -h'")
+p = Prototype()
+
+p.run()
